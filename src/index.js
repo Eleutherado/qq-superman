e8e09a942e45459b0e381e93caf51fb9b76afb82
Eleutherado <emiliov432@gmail.com>
2018-05-01 22:27:05 -0400

    added video player

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import ReactPlayer from 'react-player';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center'
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playing: false,
+      seenFirstPause: false
+    };
+
+    this.onProgress = this.onProgress.bind(this);
+    this.onPlay = this.onPlay.bind(this);
+    this.onPause = this.onPause.bind(this);
+  }
+
+  onProgress(state) {
+    const { playedSeconds, played, loadedSeconds, loaded } = state;
+    console.log(this.state.playing);
+    if (playedSeconds >= 6 && !this.state.seenFirstPause) {
+      this.setState({ playing: false, seenFirstPause: true });
+      console.log('paused!');
+    }
+  }
+
+  onPlay() {
+    this.setState({ playing: true });
+  }
+
+  onPause() {
+    this.setState({ playing: false });
+  }
+  render() {
+    return (
+      <div style={styles}>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=Rm83Q9UhJvw"
+          playing={this.state.playing}
+          onPlay={this.onPlay}
+          onPause={this.onPause}
+          onProgress={this.onProgress}
+        />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
